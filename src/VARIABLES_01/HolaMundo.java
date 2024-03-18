c1a0419979dae867142033bbefd1137ae9faa423
Yeisson <yei_rob24@hotmail.com>
2024-03-17 23:56:27 -0600

    Initial commit

---
+++ src/VARIABLES_01/HolaMundo.java
@@ -0,0 +1,7 @@
+package VARIABLES_01;
+//  3/14/2024 12:30 AM
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola, mundo!");
+    }
+}
---
+++ src/VARIABLES_01/PrimitivosFloat.java
@@ -0,0 +1,22 @@
+package VARIABLES_01;
+
+public class PrimitivosFloat {
+    public static void main(String[] args) {
+        
+        float realFloat = 3.4028235E38f;
+        System.out.println("realFloat = " + realFloat);
+        System.out.println("tipo float corresponde en bytes a = " + Float.BYTES);
+        System.out.println("tipo float corresponde en bits a = " + Float.SIZE);
+        System.out.println("valor maximo de un float = " + Float.MAX_VALUE);
+        System.out.println("valor minimo de un float = " + Float.MIN_VALUE);
+
+        double realDouble = 1.7976931348623157E308D;
+        System.out.println("\nrealDouble = " + realDouble);
+        System.out.println("tipo double corresponde en bytes a = " + Double.BYTES);
+        System.out.println("tipo double corresponde en bits a = " + Double.SIZE);
+        System.out.println("valor maximo de un double = " + Double.MAX_VALUE);
+        System.out.println("valor minimo de un double = " + Double.MIN_VALUE);
+
+
+    }
+}
